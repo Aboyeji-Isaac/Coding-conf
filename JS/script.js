523ab63ce9b96fa87acf020bd06f2693ac1d3f46
Aboyeji Isaac <aboyejiisaac@gmail.com>
2025-04-02 05:21:23 +0100

    second commit

--- JS/script.js
+++ JS/script.js
@@ -43,3 +43,30 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+
+
+const fileUpload = document.getElementById("file-upload");
+const imageContainer = document.getElementById("image-container");
+const preview = document.getElementById("preview");
+const dropzone = document.querySelector(".dropzone");
+
+fileUpload.addEventListener("change", function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            preview.src = e.target.result;
+            imageContainer.classList.remove("hidden");
+            dropzone.classList.add("hidden"); // Hide dropzone
+        };
+        reader.readAsDataURL(file);
+    }
+});
+
+function removeImage() {
+    fileUpload.value = "";
+    preview.src = "";
+    imageContainer.classList.add("hidden");
+    dropzone.classList.remove("hidden"); // Show dropzone again
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,11 +22,28 @@ module.exports = {
           'linear': 'linear-gradient(to right, #f37362, #ffffff)',
         }
       },
+      boxShadow: {
+        'default': '0 0 10px 0 rgba(0,0,0,0.1)',
+        'lg': '0 0 20px 0 rgba(0,0,0,0.1)',
+        'xl': '0 0 30px 0 rgba(0,0,0,0.1)',
+        '2xl': '0 0 40px 0 rgba(0,0,0,0.1)',
+        '3xl': '0 0 50px 0 rgba(0,0,0,0.1)',
+        '4xl': '0 0 60px 0 rgba(0,0,0,0.1)',
+      },
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+        '128':'32rem',
+        '-5': '-5px',
+        '-10': '-10px',
+        '-20': '-20px',
+      },
       fontFamily: {
         'Inconsolata': ['Inconsolata', 'monospace'],        
       }
     },
   },
   variants: {},
-  plugins: [],
+  plugins: [require("daisyui"),],
 }
